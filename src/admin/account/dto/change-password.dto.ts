dac503b30f6282852a75285c1ee951cfa3dadb0b
Lam <daibanglam@gmail.com>
2021-07-09 16:13:26 +0700

    refactor: remove error code of form validation

--- src/admin/account/dto/change-password.dto.ts
+++ src/admin/account/dto/change-password.dto.ts
@@ -1,15 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, Length } from 'class-validator';
-import { LENGTH_INPUT_ERROR, REQUIRED_INPUT_ERROR } from 'src/common/constants/error';
 
 export class ChangePasswordDto {
   @ApiProperty()
-  @Length(6, undefined, { message: LENGTH_INPUT_ERROR })
-  @IsNotEmpty({ message: REQUIRED_INPUT_ERROR })
+  @IsNotEmpty()
   currentPassword: string;
 
   @ApiProperty()
-  @Length(6, undefined, { message: LENGTH_INPUT_ERROR })
-  @IsNotEmpty({ message: REQUIRED_INPUT_ERROR })
+  @Length(6)
+  @IsNotEmpty()
   newPassword: string;
 }
--- src/diary/activity/dto/add-activity.dto.ts
+++ src/diary/activity/dto/add-activity.dto.ts
@@ -1,14 +1,13 @@
 import { IsNotEmpty, IsOptional, IsPositive } from 'class-validator';
-import { REQUIRED_INPUT_ERROR } from 'src/common/constants/error';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AddActivityDto {
   @ApiProperty()
-  @IsNotEmpty({ message: REQUIRED_INPUT_ERROR })
+  @IsNotEmpty()
   content: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: REQUIRED_INPUT_ERROR })
+  @IsNotEmpty()
   time: string;
 
   @ApiProperty()
