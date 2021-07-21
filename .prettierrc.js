8139deeb98faa64994e110275b3abec4ee33e462
Lam <daibanglam@gmail.com>
2021-07-21 17:02:33 +0700

    feat: configure ESLint

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  trailingComma: "all",
-  singleQuote: true,
-  tabWidth: 2,
-  printWidth: 100,
-  semi: true
-};
--- data/setup.js
+++ data/setup.js
@@ -1,3 +1,4 @@
+/* eslint-env mongo */
 // this file contain javascript code to be executed in mongo shell
 // resposible for setting up mongo database
 
