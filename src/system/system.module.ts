7621b17ad07eebe675dc61e31e3e211bf8092170
Lam <daibanglam@gmail.com>
2021-09-28 17:40:05 +0700

    chore: remove unused code

--- src/system/system.module.ts
+++ src/system/system.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SettingController } from './setting/setting.controller';
 import { SettingService } from './setting/setting.service';
-import { UploadController } from './upload/upload.controller';
-import { UploadService } from './upload/upload.service';
 
 @Module({
-  controllers: [SettingController, UploadController],
-  providers: [SettingService, UploadService],
+  controllers: [SettingController],
+  providers: [SettingService],
 })
 export class SystemModule {}
