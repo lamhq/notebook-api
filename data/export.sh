25d86d90cd3c7b91fc5da9e84e270fa91904a6c2
Lam <daibanglam@gmail.com>
2022-03-12 11:25:58 +0700

    chore: update data script

--- data/export.sh
+++ data/export.sh
@@ -5,3 +5,5 @@ cd "$(dirname "$0")"
 DB_URI=mongodb://localhost:27017/notebook
 
 mongoexport --uri ${DB_URI} -c administrators --out administrators.json --jsonArray --pretty
+mongoexport --uri ${DB_URI} -c activities --out activities.json --jsonArray --pretty
+mongoexport --uri ${DB_URI} -c tags --out tags.json --jsonArray --pretty
--- data/import.sh
+++ data/import.sh
@@ -5,5 +5,7 @@ cd "$(dirname "$0")"
 DB_URI='mongodb://localhost:27017/notebook'
 
 mongoimport --uri ${DB_URI} --collection administrators --file administrators.json --jsonArray --drop
+mongoimport --uri ${DB_URI} --collection activities --file activities.json --jsonArray --drop
+mongoimport --uri ${DB_URI} --collection tags --file tags.json --jsonArray --drop
 
 mongo ${DB_URI} < ./setup.js
