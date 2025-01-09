daf0dfc3575e0a1cd1a7565089eecf103acd767c
Lam <daibanglam@gmail.com>
2025-01-09 09:21:32 +0700

    chore: remove unused code

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { configFactory } from './config';
 import { DiaryModule } from './diary/diary.module';
 
 @Module({
-  imports: [DiaryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // allow injecting ConfigService in module factory
+      load: [configFactory],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const typeormConfig = configService.get<TypeOrmModuleOptions>('typeorm');
+        if (!typeormConfig) {
+          throw new Error(
+            'Invalid system configuration. TypeORM config is not set.',
+          );
+        }
+        return typeormConfig;
+      },
+    }),
+    EventEmitterModule.forRoot({
+      wildcard: true,
+    }),
+    DiaryModule,
+  ],
 })
 export class AppModule {}
--- src/api/diary/diary.module.ts
+++ src/api/diary/diary.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { EventEmitterModule } from '@nestjs/event-emitter';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActivityController } from './activity/activity.controller';
 import { Activity } from './activity/activity.entity';
 import { ActivityService } from './activity/activity.service';
-import { configFactory } from './config';
 import { StatController } from './stat/stat.controller';
 import { StatService } from './stat/stat.service';
 import { TagController } from './tag/tag.controller';
@@ -13,28 +10,7 @@ import { Tag } from './tag/tag.entity';
 import { TagService } from './tag/tag.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // allow injecting ConfigService in module factory
-      load: [configFactory],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const typeormConfig = configService.get<TypeOrmModuleOptions>('typeorm');
-        if (!typeormConfig) {
-          throw new Error(
-            'Invalid system configuration. TypeORM config is not set.',
-          );
-        }
-        return typeormConfig;
-      },
-    }),
-    EventEmitterModule.forRoot({
-      wildcard: true,
-    }),
-    TypeOrmModule.forFeature([Activity, Tag]),
-  ],
+  imports: [TypeOrmModule.forFeature([Activity, Tag])],
   controllers: [ActivityController, TagController, StatController],
   providers: [ActivityService, TagService, StatService],
 })
